8353e781f30b2171965256da7792dd7ee0b17fc3
maxime lemoine <maxime.lemoine76@gmail.com>
2023-12-16 20:37:33 +0100

    feat(authorize-location): add authorize location page in mobile web #11

--- apps/web/src/app/(auth)/register/page.tsx
+++ apps/web/src/app/(auth)/register/page.tsx
@@ -1,12 +1,24 @@
+"use client"
+
 import AuthContainer from "@/components/Auth/AuthContainer"
 import Button from "@/components/Button/Button"
 import FormInput from "@/components/Form/FormInput"
+import AuthorizeLocation from "@/components/Location/AuthorizeLocation"
+import { useState } from "react"
 
 const Register = () => {
+
+    const [send, setSend] = useState(false)
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setSend(true)
+    }
+
     return (
         <div>
             <AuthContainer pageTitle="Sign Up" pageDescription="Please sign up to get started" allowBack={true}>
-                <form className="flex flex-col justify-between h-full">
+                <form className="flex flex-col justify-between h-full" onSubmit={handleSubmit}>
                     <div>
                         <FormInput value="" name="text" label="Name" type="text" placeholder="John doe" />
                         <FormInput value="" name="email" label="Email" type="email" placeholder="example@gmail.com" />
@@ -16,6 +28,10 @@ const Register = () => {
                     <Button type="submit" className="mt-7">Sign Up</Button>
                 </form>
             </AuthContainer>
+
+            {send === true ? (
+                <AuthorizeLocation />
+            ) : null}
         </div>
     )
 }
