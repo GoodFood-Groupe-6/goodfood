056ab71cdf0cfece2faa924e2ffa1e2c9f0c150f
maxime lemoine <maxime.lemoine76@gmail.com>
2024-01-03 18:23:05 +0100

    feat(forgot-passwordd): add forgot password page in desktop #19

--- apps/web/src/components/Location/AuthorizeLocation.tsx
+++ apps/web/src/components/Location/AuthorizeLocation.tsx
@@ -6,8 +6,8 @@ const AuthorizeLocation = () => {
     return (
         <div className="w-screen h-screen absolute top-0 left-0 bg-white flex flex-col items-center justify-center p-6 overflow-hidden">
             <div className="bg-[#98A8B8] w-52 h-64 rounded-3xl"></div>
-            <Button className="mt-24 text-base">Access location</Button>
-            <span className="text-[#646982] text-base uppercase text-center mt-9">goodfood will access your location only while using the app</span>
+            <Button className="mt-24 text-base lg:w-64">Access location</Button>
+            <span className="text-[#646982] text-base uppercase text-center mt-9 lg:w-1/4">goodfood will access your location only while using the app</span>
         </div>
     )
 }
