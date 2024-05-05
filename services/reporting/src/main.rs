d2c244345a6b226d3acc5e96db9e18f1ffea2231
Gregory <gregory.leblond.dev@gmail.com>
2024-05-05 19:55:21 +0200

    feat(reporting): create main file

--- services/reporting/src/main.rs
+++ services/reporting/src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use axum::extract::{Path, Query};
+use axum::response::{Html, IntoResponse, Response};
+use axum::routing::{get, get_service};
+use axum::{middleware, Router};
+use serde::Deserialize;
+use std::net::SocketAddr;
+use tower_cookies::CookieManagerLayer;
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() {
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .layer(middleware::map_response(main_response_mapper))
+        .layer(CookieManagerLayer::new())
+        .fallback_service(routes_static());
+
+    // Start the server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(routes_all.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[derive(Deserialize, Debug)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+fn routes_hello() -> Router {
+    return Router::new().route("/test", get(handler_test));
+}
+
+async fn handler_test(Query(params): Query<HelloParams>) -> Html<&'static str> {
+    println!("Appel route /test");
+
+    let name = params.name.unwrap_or("World".to_string());
+
+    Html("<p>Hello, World!</p>")
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+async fn main_response_mapper(res: Response) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+
+    println!();
+    res
 }
