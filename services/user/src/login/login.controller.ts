c001b8fc22add4ebfbc778ee57294864e91e7459
Gregory LEBLOND <gregory.leblond.dev@gmail.com>
2024-09-01 17:48:24 +0200

    fix: add /api to route path

--- services/user/src/login/login.controller.ts
+++ services/user/src/login/login.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreateLoginDto } from './dto/create-login.dto';
 import { LoginService } from './login.service';
 
-@Controller('login')
+@Controller('/api/login')
 export class LoginController {
   constructor(private readonly loginService: LoginService) {}
 
